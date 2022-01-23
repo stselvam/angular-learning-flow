9e55a7d1e96c4284617f003c511241e7cee5b050
ST Selvam <tselvam.sts@gmail.com>
2022-01-23 15:39:52 +0530

    Jan 22 commit

--- inventory-app/src/app/app.component.ts
+++ inventory-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'inventory-app-root',
+  template: `
+    <div>
+      (Products will go here soon)
+    </div>
+  `,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
 }
---
+++ inventory-app/src/app/product.model.ts
@@ -0,0 +1,9 @@
+export class product {
+    constructor () {
+        public sku: string;
+        public name: string;
+        public imageUrl: string;
+        public department: string[];
+        public price: number;
+    }
+}
